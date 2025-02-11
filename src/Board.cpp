766241b6ae75bd5076292870fe320d3dfc6a9744
anne passelegue <annepas69@gmail.com>
2025-02-11 10:40:07 +0100

    Pions de la bonne couleur

--- src/Board.cpp
+++ src/Board.cpp
@@ -9,41 +9,41 @@
 #include "./pieces/Queen.hpp"
 
 void Board::init_board() {
-    this->positions_board[get_pos_1D(std::make_pair(0,0))] = new Rook(Color::White); 
-    this->positions_board[get_pos_1D(std::make_pair(0,1))] = new Knight(Color::White); 
-    this->positions_board[get_pos_1D(std::make_pair(0,2))] = new Bishop(Color::White); 
-    this->positions_board[get_pos_1D(std::make_pair(0,3))] = new Queen(Color::White); 
-    this->positions_board[get_pos_1D(std::make_pair(0,4))] = new King(Color::White); 
-    this->positions_board[get_pos_1D(std::make_pair(0,5))] = new Bishop(Color::White); 
-    this->positions_board[get_pos_1D(std::make_pair(0,6))] = new Knight(Color::White); 
-    this->positions_board[get_pos_1D(std::make_pair(0,7))] = new Rook(Color::White); 
+    this->positions_board[get_pos_1D(std::make_pair(0,0))] = new Rook(Color::Black); 
+    this->positions_board[get_pos_1D(std::make_pair(0,1))] = new Knight(Color::Black); 
+    this->positions_board[get_pos_1D(std::make_pair(0,2))] = new Bishop(Color::Black); 
+    this->positions_board[get_pos_1D(std::make_pair(0,3))] = new Queen(Color::Black); 
+    this->positions_board[get_pos_1D(std::make_pair(0,4))] = new King(Color::Black); 
+    this->positions_board[get_pos_1D(std::make_pair(0,5))] = new Bishop(Color::Black); 
+    this->positions_board[get_pos_1D(std::make_pair(0,6))] = new Knight(Color::Black); 
+    this->positions_board[get_pos_1D(std::make_pair(0,7))] = new Rook(Color::Black); 
     
-    this->positions_board[get_pos_1D(std::make_pair(1,0))] = new Pawn(Color::White);
-    this->positions_board[get_pos_1D(std::make_pair(1,1))] = new Pawn(Color::White);
-    this->positions_board[get_pos_1D(std::make_pair(1,2))] = new Pawn(Color::White);
-    this->positions_board[get_pos_1D(std::make_pair(1,3))] = new Pawn(Color::White);
-    this->positions_board[get_pos_1D(std::make_pair(1,4))] = new Pawn(Color::White);
-    this->positions_board[get_pos_1D(std::make_pair(1,5))] = new Pawn(Color::White);
-    this->positions_board[get_pos_1D(std::make_pair(1,6))] = new Pawn(Color::White);
-    this->positions_board[get_pos_1D(std::make_pair(1,7))] = new Pawn(Color::White); 
+    this->positions_board[get_pos_1D(std::make_pair(1,0))] = new Pawn(Color::Black);
+    this->positions_board[get_pos_1D(std::make_pair(1,1))] = new Pawn(Color::Black);
+    this->positions_board[get_pos_1D(std::make_pair(1,2))] = new Pawn(Color::Black);
+    this->positions_board[get_pos_1D(std::make_pair(1,3))] = new Pawn(Color::Black);
+    this->positions_board[get_pos_1D(std::make_pair(1,4))] = new Pawn(Color::Black);
+    this->positions_board[get_pos_1D(std::make_pair(1,5))] = new Pawn(Color::Black);
+    this->positions_board[get_pos_1D(std::make_pair(1,6))] = new Pawn(Color::Black);
+    this->positions_board[get_pos_1D(std::make_pair(1,7))] = new Pawn(Color::Black); 
     
-    this->positions_board[get_pos_1D(std::make_pair(6,0))] = new Pawn(Color::Black);
-    this->positions_board[get_pos_1D(std::make_pair(6,1))] = new Pawn(Color::Black);
-    this->positions_board[get_pos_1D(std::make_pair(6,2))] = new Pawn(Color::Black);
-    this->positions_board[get_pos_1D(std::make_pair(6,3))] = new Pawn(Color::Black);
-    this->positions_board[get_pos_1D(std::make_pair(6,4))] = new Pawn(Color::Black);
-    this->positions_board[get_pos_1D(std::make_pair(6,5))] = new Pawn(Color::Black);
-    this->positions_board[get_pos_1D(std::make_pair(6,6))] = new Pawn(Color::Black);
-    this->positions_board[get_pos_1D(std::make_pair(6,7))] = new Pawn(Color::Black);  
+    this->positions_board[get_pos_1D(std::make_pair(6,0))] = new Pawn(Color::White);
+    this->positions_board[get_pos_1D(std::make_pair(6,1))] = new Pawn(Color::White);
+    this->positions_board[get_pos_1D(std::make_pair(6,2))] = new Pawn(Color::White);
+    this->positions_board[get_pos_1D(std::make_pair(6,3))] = new Pawn(Color::White);
+    this->positions_board[get_pos_1D(std::make_pair(6,4))] = new Pawn(Color::White);
+    this->positions_board[get_pos_1D(std::make_pair(6,5))] = new Pawn(Color::White);
+    this->positions_board[get_pos_1D(std::make_pair(6,6))] = new Pawn(Color::White);
+    this->positions_board[get_pos_1D(std::make_pair(6,7))] = new Pawn(Color::White);  
 
-    this->positions_board[get_pos_1D(std::make_pair(7,0))] = new Rook(Color::Black); 
-    this->positions_board[get_pos_1D(std::make_pair(7,1))] = new Knight(Color::Black); 
-    this->positions_board[get_pos_1D(std::make_pair(7,2))] = new Bishop(Color::Black);
-    this->positions_board[get_pos_1D(std::make_pair(7,3))] = new Queen(Color::Black);  
-    this->positions_board[get_pos_1D(std::make_pair(7,4))] = new King(Color::Black); 
-    this->positions_board[get_pos_1D(std::make_pair(7,5))] = new Bishop(Color::Black); 
-    this->positions_board[get_pos_1D(std::make_pair(7,6))] = new Knight(Color::Black); 
-    this->positions_board[get_pos_1D(std::make_pair(7,7))] = new Rook(Color::Black); 
+    this->positions_board[get_pos_1D(std::make_pair(7,0))] = new Rook(Color::White); 
+    this->positions_board[get_pos_1D(std::make_pair(7,1))] = new Knight(Color::White); 
+    this->positions_board[get_pos_1D(std::make_pair(7,2))] = new Bishop(Color::White);
+    this->positions_board[get_pos_1D(std::make_pair(7,3))] = new Queen(Color::White);  
+    this->positions_board[get_pos_1D(std::make_pair(7,4))] = new King(Color::White); 
+    this->positions_board[get_pos_1D(std::make_pair(7,5))] = new Bishop(Color::White); 
+    this->positions_board[get_pos_1D(std::make_pair(7,6))] = new Knight(Color::White); 
+    this->positions_board[get_pos_1D(std::make_pair(7,7))] = new Rook(Color::White); 
 }
 
 void Board::set_piece(Piece *piece, int position) {
