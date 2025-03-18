a05fad75169db9005764670f8daeb7b6e3462617
anne passelegue <annepas69@gmail.com>
2025-03-18 09:48:13 +0100

    Import librairie open-gl

---
+++ src/3d/test.hpp
@@ -0,0 +1,75 @@
+#pragma once
+#define GLFW_INCLUDE_NONE
+#include <GLFW/glfw3.h>
+#include <glad/glad.h>
+
+int test_triangle()
+{
+    /* Initialize the library */
+    if (!glfwInit()) {
+        return -1;
+    }
+    GLFWwindow* window = glfwCreateWindow(800, 800, "test_triangle", nullptr, nullptr);
+    if (!window) {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    /* Intialize glad (loads the OpenGL functions) */
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        return -1;
+    }
+
+    /*********************************
+     * HERE SHOULD COME THE INITIALIZATION CODE
+     *********************************/
+    // Le VBO contient les données
+    GLuint vbo;
+    glGenBuffers(1, &vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+        GLfloat vertices[] = { -0.5f, -0.5f, 0.5f, -0.5f, 0.0f, 0.5f };
+        glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(GLfloat), vertices, GL_STATIC_DRAW);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+    // Le VAO décrit les données
+    GLuint vao;
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
+        const GLuint VERTEX_ATTR_POSITION = 0;
+        glEnableVertexAttribArray(VERTEX_ATTR_POSITION);
+        glBindBuffer(GL_ARRAY_BUFFER, vbo);
+            glVertexAttribPointer(VERTEX_ATTR_POSITION, 2, GL_FLOAT, GL_FALSE, 2* sizeof(GLfloat), nullptr);
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+    
+    
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window)) {
+        glClearColor(1.f, 0.5f, 0.5f, 1.f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        /*********************************
+        * HERE SHOULD COME THE RENDERING CODE
+        *********************************/
+       // Nettoyer l'écran
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glBindVertexArray(vao);
+            glDrawArrays(GL_TRIANGLES, 0, 3);
+        glBindVertexArray(0);
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    // Libérer la mémoire
+    glDeleteBuffers(1, &vbo);
+    glDeleteVertexArrays(1, &vao);
+
+    glfwTerminate();
+    return 0;
+}
